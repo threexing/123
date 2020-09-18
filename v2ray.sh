52cdfd1545b6959a03cf9ed755bea9917c5a0ea4
threexing <62030450+threexing@users.noreply.github.com>
2020-09-19 01:33:31 +0800

    Create v2ray.sh

---
+++ v2ray.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# config caddy
+
+cat << EOF > /etc/caddy/Caddyfile
+
+:$PORT
+
+root * /usr/share/caddy
+
+file_server
+
+@websockets_heroku {
+
+header Connection *Upgrade*
+
+header Upgrade    websocket
+
+path $WSPATH
+
+}
+
+reverse_proxy @websockets_heroku 127.0.0.1:10086
+
+EOF
+
+# config v2ray
+
+cat << EOF > /usr/bin/v2ray/config.json
+
+{
+
+    "inbounds": 
+
+    [
+
+        {
+
+            "port": 10086,"listen": "127.0.0.1","protocol": "vless",
+
+            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},
+
+            "streamSettings": {"network": "ws","wsSettings": {"path": "$WSPATH"}}
+
+        }
+
+    ],
+
+	    "outbounds": 
+
+    [
+
+        {"protocol": "freedom","tag": "direct","settings": {}},
+
+        {"protocol": "socks","tag": "sockstor","settings": {"servers": [{"address": "127.0.0.1","port": 9050}]}},
+
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+
+    ],
+
+	
+
+    "routing": 
+
+    {
+
+        "rules": 
+
+        [
+
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private","geoip:cn"]},
+
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:private","geosite:cn","geosite:category-ads-all"]},
+
+            {"type": "field","outboundTag": "sockstor","domain": ["geosite:tor"]}
+
+        ]
+
+    }
+
+}	
+
+EOF
+
+# start tor v2ray
+
+nohup tor &
+
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
+
+/usr/bin/v2ray/v2ray -config /usr/bin/v2ray/config.json
